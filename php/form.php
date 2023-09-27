40b959908579b78b77fdb852ea20f8a9aec7eeed
Moustache The Cat <joubert.mathieu753783@gmail.com>
2023-09-27 18:58:44 +0200

    save before reset all css

--- php/form.php
+++ php/form.php
@@ -7,14 +7,18 @@ if($_POST['submit']= "Envoyer"){
     $last_name = $_POST['last-name'];  
     $number = $_POST['number'];
     $message_demande = $_POST['message']; 
+    $send_copie = $_POST['send-copie'];
     $subject = "Demande de contact de". " " . $last_name." ". $first_name;
-    // $subject2 = "Copy of your form submission"; 
+    $subject2 = "Copie de votre demande de contact sur le portofolio du chat"; 
     $message = $first_name . " " . $last_name . " ". $_POST['number'] . " wrote the following:" . "\n\n" . $_POST['message']; 
-    // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message']; 
+    $message2 = "Voici la copie de votre demande" . $first_name . "\n\n" . $_POST['message']; 
     $headers = "From:" . $from; 
-    // $headers2 = "From:" . $to; 
+    $headers2 = "From:" . $to; 
     mail($to,$subject,$message,$headers); 
-    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender 
+    if (isset($send_copie)){
+        mail($from,$subject2,$message2,$headers2);
+    }
+    
     $response = "Votre message a bien été envoyé";
     header('Location: ../index.html'); 
 }else{
